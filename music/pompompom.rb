e65b1306069aa9bdcb9377de69e5c5af90993b89
Lauobrio42 <laurine.obriotbouvier@outlook.fr>
2019-11-06 17:30:37 +0100

    Ajouts sooons

--- music/pompompom.rb
+++
@@ -1,4 +0,0 @@
-live_loop :scale_player do
-  play (scale :Eb3, :super_locrian).tick, release: 0.1, amp: 0.2
-  sleep 0.25
-end
--- music/symbales_début_jeu.rb
+++
@@ -1,9 +0,0 @@
-live_loop :cym do
-  use_sample_defaults  release: 0.8
-  sample :drum_cymbal_closed, amp: 0.1
-  with_fx :bitcrusher, sample_rate: rrand(1000, 32000), bits: rrand(8, 12), pre_amp: 0.8, amp: 0.4,  mix: 0.8, cutoff: 130 do
-    use_sample_defaults  release: 0.4
-    sample :drum_cymbal_soft
-    sleep  0.5
-  end
-end
